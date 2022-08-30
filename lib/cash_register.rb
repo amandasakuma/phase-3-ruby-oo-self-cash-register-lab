d73ec800dadbc86ebe95d209d6251b69ff6001e2
Amanda Sakuma <amandasakuma@gmail.com>
2022-08-30 16:34:02 -0400

    commit tree 882896b73a5685d5650c18346f5d1d317d812136

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
+    attr_accessor :discount, :total, :prev_total, :items
 
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.prev_total = price * quantity
+        self.total += self.prev_total
+        quantity.times do   
+            self.items << title
+        end
+    end
+
+end
